6b880eb5a567484a6701bf96d4530864fb88a8ce
Cristóbal Medina López <cristobalmedinalopez@gmail.com>
2016-10-18 13:58:40 +0200

    initial commit

---
+++ peer.js
@@ -0,0 +1,167 @@
+/*
+GNU GENERAL PUBLIC LICENSE
+
+This is an example of a client-side for a multi-party chat over WebRTC.
+
+Copyright (C) 2014 Cristóbal Medina López.
+http://www.p2psp.org
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+var signalingChannel = new WebSocket("ws://127.0.0.1:9876/");
+var configuration = {iceServers: [{ url: 'stun:stun.l.google.com:19302' }]};
+//var configuration = {iceServers: [{ url: 'stun:150.214.150.137:3478' }]};
+var pcs=[];
+var channel=[];
+var peerlist=[];
+var idpeer=0;
+var iniConnection = document.getElementById("initiateConnection");
+var msg = document.getElementById("msg");
+var btnSend= document.getElementById("send");
+btnSend.disabled=true;
+
+iniConnection.onclick=function(e){
+	document.getElementById("receive").innerHTML="<b>Conecting...</b>";
+	for (i in peerlist){
+		start(true,peerlist[i]);
+	}
+	iniConnection.disabled=true;
+};
+
+btnSend.onclick=sendChatMessage;
+
+// call start(true,i) to initiate
+function start(isInitiator,i) {
+     //iniConnection.disabled=true;
+	console.log("creado para: "+i);
+	pcs[i] = new webkitRTCPeerConnection(configuration, {optional: []});
+	
+
+	// send any ice candidates to the other peer
+	pcs[i].onicecandidate = function (evt) {
+		if (evt.candidate){
+			signalingChannel.send(JSON.stringify({ "candidate": evt.candidate , "idtransmitter":'"'+idpeer+'"', "idreceiver":'"'+i+'"'}));
+		}
+	};
+
+
+	// let the "negotiationneeded" event trigger offer generation
+	pcs[i].onnegotiationneeded = function () {
+		pcs[i].createOffer(function(desc){localDescCreated(desc,pcs[i],i);});
+		console.log("Create and send OFFER");
+	}
+
+	if (isInitiator) {
+		// create data channel and setup chat
+		channel[i] = pcs[i].createDataChannel("chat"+i);
+		setupChat(i);
+	} else {
+	// setup chat on incoming data channel
+		pcs[i].ondatachannel = function (evt) {
+			channel[i] = evt.channel;
+			setupChat(i);
+		};
+	}  
+	console.log("Saved in slot: "+i+" PeerConection: "+pcs[i]);
+}
+
+function localDescCreated(desc,pc,i) {
+    pc.setLocalDescription(desc, function () {
+	console.log("localDescription is Set");
+        signalingChannel.send(JSON.stringify({ "sdp": pc.localDescription , "idtransmitter":'"'+idpeer+'"', "idreceiver":'"'+i+'"'}));
+    }, logError);
+}
+
+signalingChannel.onmessage = function (evt) {
+	handleMessage(evt);
+}
+
+function handleMessage(evt){
+	var message = JSON.parse(evt.data);
+	
+    if (message.numpeer){    
+		idpeer=message.numpeer;
+		console.log('Peer ID: '+idpeer);
+		return;  	
+    }  
+
+	if (message.peerlist){    
+		console.log('Peer List '+message.peerlist);
+		peerlist=JSON.parse(message.peerlist);
+		for (i in peerlist){
+			console.log("Peer: "+peerlist[i]);
+		}
+		return;  	
+    }  
+   
+    var id=(message.idtransmitter).split('"').join(''); 
+	var idreceiver=(message.idreceiver).split('"').join(''); 
+    console.log("Received from: "+id+" and send to: "+idreceiver);
+
+    if (!pcs[id]) { 
+		console.log('%cCreate a new PeerConection','background: #222; color: #bada55');
+		peerlist.push(id);
+		console.log("PEER LIST UPDATE: "+peerlist);
+		start(false,id);
+    } 	
+
+    if (message.sdp && idreceiver==idpeer){
+        //console.log(message.sdp);   
+	pcs[id].setRemoteDescription(new RTCSessionDescription(message.sdp), function () {
+		console.log("remoteDescription is Set");
+    		// if we received an offer, we need to answer
+		if (pcs[id].remoteDescription.type == "offer"){
+			console.log("Create and send ANSWER");
+        		pcs[id].createAnswer(function(desc){localDescCreated(desc,pcs[id],id);});
+    		}
+        });
+    }
+
+    if (message.candidate && idreceiver==idpeer){
+		console.log("Received ice candidate: "+ message.candidate.candidate); 
+		pcs[id].addIceCandidate(new RTCIceCandidate(message.candidate));
+    }
+
+}
+
+function setupChat(i) {
+    channel[i].onopen = function () {
+        btnSend.disabled=false;
+	document.getElementById("chatcontrols").style.display="inline";
+    };
+
+    channel[i].onmessage = function (evt) {
+       document.getElementById("receive").innerHTML+="<br />"+evt.data;
+    };
+}
+
+function sendChatMessage() {
+    document.getElementById("receive").innerHTML+="<br />"+document.getElementById("login").value+ ": "+msg.value;
+	for (i in peerlist){  
+		if (peerlist[i]!=idpeer){
+			console.log("send to "+peerlist[i]);  
+			try{
+				channel[peerlist[i]].send(document.getElementById("login").value+ ": "+msg.value);
+			}catch(e){
+				console.log(i+" said bye!");
+			}
+
+		}
+	}
+}
+
+function logError(error) {
+    console.log(error.name + ": " + error.message);
+}
---
+++ server.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python -O
+# -*- coding: iso-8859-15 -*-
+
+'''
+GNU GENERAL PUBLIC LICENSE
+
+This is an example of a signaling server for a multi-party chat over WebRTC.
+
+Copyright (C) 2014 Cristóbal Medina López.
+http://www.p2psp.org
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+import socket, threading, time, base64, hashlib, struct, binascii
+import json
+from SimpleWebSocketServer import WebSocket, SimpleWebSocketServer
+
+class Signaling(WebSocket):
+
+        def handleMessage(self):
+		datos=str(self.data)
+		try:
+		    decoded = json.loads(datos)
+		except (ValueError, KeyError, TypeError):
+		    print "JSON format error"
+
+		if 'sdp' in decoded:		
+			if decoded['sdp']['type'] == 'offer':
+				print 'Offer'
+			else:
+				print 'Answer'
+
+		if 'candidate' in decoded:
+			print 'Candidate num: '+decoded['idtransmitter']
+
+		for client in self.server.connections.itervalues():
+					if client != self:
+						try:
+							client.sendMessage(str(self.data))
+						except Exception as n:
+							print n
+
+
+	def handleConnected(self):
+		global nextid
+		print self.address, 'connected'
+
+		try:
+			self.sendMessage(str('{"numpeer":"'+str(nextid)+'"}'))
+			self.sendMessage(str('{"peerlist":"'+str(peerlist)+'"}'))
+			peerlist.append(nextid)
+			peeridlist[nextid]=self
+			nextid=nextid+1
+		except Exception as n:
+			print n
+
+	def handleClose(self):
+		print self.address, 'closed'
+		peerlist.remove(peeridlist[self]);
+		'''for client in self.server.connections.itervalues():
+			if client != self:
+				try:
+					client.sendMessage(str(self.address[0]) + ' - disconnected')
+				except Exception as n:
+					print n '''       
+
+if __name__ == '__main__':
+	nextid = 0
+	peerlist=[]
+	peeridlist={}
+	server = SimpleWebSocketServer('', 9876, Signaling)
+	server.serveforever()
+
